99bdc08203e9ddfb8a3b87aa6764cb5ee4bfeaad
ishanaryan1602 <github1602@gmail.com>
2024-07-31 15:57:59 +0530

    Setup files for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar"
+import Home from "./Pages/Home/Home"
+import Cart from "./Pages/Cart/Cart"
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder"
 
 function App() {
 
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+
+      </Routes>
     </div>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favorite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eaque minima alias perspiciatis, doloremque nihil laudantium atque sunt adipisci delectus.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header

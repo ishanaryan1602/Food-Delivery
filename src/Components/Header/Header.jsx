706f7732900654be1f7092fa6e6f52ee769b0acb
ishanaryan1602 <github1602@gmail.com>
2024-08-01 21:26:54 +0530

    Menu frontend and logic

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import './Header.css'
 
 const Header = () => {
-  return (
-    <div className='header'>
-        <div className="header-contents">
-            <h2>Order your favorite food here</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eaque minima alias perspiciatis, doloremque nihil laudantium atque sunt adipisci delectus.</p>
-            <button>View Menu</button>
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order your favorite food here</h2>
+                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <button>View Menu</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
